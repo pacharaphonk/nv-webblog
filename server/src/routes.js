7cc772e7e26faa0a1d06738f04ab0229e538eb98
pacharaphonk <pacharaphonk62@nu.ac.th>
2021-09-03 21:45:41 +0700

    Update

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,6 +1,6 @@
 const UserController = require('./controllers/UserController')
 const UserAuthenController = require('./controllers/UserAuthenController')
-const isAuthenController = require('./authen/isAuthenController')//ลิ้งนี้ทำให้ไม่สามารถโชว์สมาชิกได้
+const isAuthenController = require('./authen/isAuthenController')
 const BlogController = require('./controllers/BlogController')
 const CommentController = require('./controllers/CommentController')
 
@@ -9,7 +9,7 @@ module.exports = (app) => {
   app.put('/user/:userId',UserController.put)
   app.delete('/user/:userId',UserController.remove)
   app.get('/user/:userId',UserController.show)
-  app.get('/users',isAuthenController, UserController.index)//นี่อีก isAuthenController
+  app.get('/users',isAuthenController, UserController.index)
   //login
   app.post('/login',UserAuthenController.login)
   //blog
@@ -19,9 +19,9 @@ module.exports = (app) => {
   app.get('/blog/:blogId',BlogController.show)
   app.get('/blogs',BlogController.index)
   //comment
-  app.post('./comment',CommentController.create)
-  app.put('./comment/:commentId',CommentController.put)
-  app.delete('./comment/:commentId',CommentController.remove)
-  app.get('./comment/:commentId',CommentController.show)
-  app.get('./comments',CommentController.index)
+  app.post('/comment', CommentController.create)
+  app.put('/comment/:commentId',CommentController.put)
+  app.delete('/comment/:commentId',CommentController.remove)
+  app.get('/comment/:commentId',CommentController.show)
+  app.get('/comments',CommentController.index)
 }
